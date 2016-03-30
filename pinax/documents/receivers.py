c73572f2a9b63d35daf8b5935c4a1e6a0422c122
Ethan A Kent <ethanallenkent@gmail.com>
2016-03-30 12:16:57 -0700

    Implement deletion of file object via Document model pre_save signal.

--- pinax/documents/receivers.py
+++ pinax/documents/receivers.py
@@ -1,8 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from .conf import settings
-from .models import UserStorage
+from .models import UserStorage, Document
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
@@ -10,3 +10,10 @@ def ensure_userstorage(sender, **kwargs):
     if kwargs["created"]:
         user = kwargs["instance"]
         UserStorage.objects.create(user=user, bytes_total=(1024 * 1024 * 50))
+
+
+# Receive the pre_delete signal and delete the file associated with the model instance.
+@receiver(pre_delete, sender=Document)
+def document_delete(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    instance.file.delete(False)
