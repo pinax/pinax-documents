94a77e38db988b466dd2b3c115725515e8b6c019
Graham Ullrich <graham@flyingcracker.com>
2018-03-17 14:49:01 -0600

    Remove django-user-accounts dependency
    Use DUA if it is available, but no longer require it.
    If using pinax-templates, version 2.0.0+ is required.

--- pinax/documents/compat.py
+++ pinax/documents/compat.py
@@ -4,3 +4,8 @@ try:
 except ImportError:
     # Python 3
     from itertools import zip_longest as izip_longest  # noqa
+
+try:
+    from account.mixins import LoginRequiredMixin
+except ImportError:
+    from django.contrib.auth.mixins import LoginRequiredMixin  # noqa
--- pinax/documents/templatetags/pinax_documents_tags.py
+++ pinax/documents/templatetags/pinax_documents_tags.py
@@ -1,4 +1,5 @@
 from django.template import Library
+from django.utils.html import conditional_escape
 
 from ..utils import convert_bytes
 
@@ -15,3 +16,13 @@ def can_share(member, user):
 @register.filter
 def readable_bytes(bytes):
     return convert_bytes(bytes)
+
+
+@register.simple_tag
+def user_display(user):
+    try:
+        # Use django-user-accounts display function if available
+        from account.utils import user_display as account_user_display
+        return conditional_escape(account_user_display(user))
+    except ImportError:
+        return conditional_escape(user.username)
